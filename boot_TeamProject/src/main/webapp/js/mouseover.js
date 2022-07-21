6414c79695bbbbe547fecd446c15d7a6cdf838aa
gayoung <lirkdud8190@gmail.com>
2022-07-21 21:39:02 +0900

    update preview ver 2.1

--- boot_TeamProject/src/main/webapp/js/mouseover.js
+++
@@ -1,7 +0,0 @@
-$(document).mouseup(function (e){
-    var LayerPopup = $("#layer_bg");
-    if(LayerPopup.has(e.target).length === 0){
-        document.getElementById('layer_bg').style = 'display:none';
-        $("body").css("overflow","scroll");
-    }
-});
