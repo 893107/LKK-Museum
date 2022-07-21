f45f21d8aabb3007855dd46aaae9064f7b737d93
jwkim <kml1763@gmail.com>
2022-07-21 20:37:51 +0900

    coverting project ver2.0

---
+++ boot_Team_coverting/src/main/java/com/example/demo/controller/api/AdminApiController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.config.auth.PrincipalDetail;
+import com.example.demo.model.RoleType;
+import com.example.demo.model.User;
+import com.example.demo.service.AdminService;
+
+@RequiredArgsConstructor
+@RestController
+public class AdminApiController {
+
+    private final AdminService adminService;
+
+
+    // 관리자페이지에서 멤버 등급 바꾸기
+    @PutMapping("/admin/manage/member/edit/{id}")
+    public ResponseEntity<?> changeRole(@RequestBody User roleUser, @PathVariable("id") Integer id, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        User user = principalDetail.getUser();
+        if (user.getRole() == RoleType.ADMIN) {
+            System.out.println("컨트롤러 role = " + roleUser);
+            adminService.changeRole(id, roleUser);
+
+            return new ResponseEntity<>(1, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(1, HttpStatus.FORBIDDEN);
+        }
+
+    }
+}
---
+++ boot_Team_coverting/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,30 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.User;
+
+import java.util.Optional;
+
+// DAO
+// 자동으로 bean 등록
+// @Repository 생략가능
+
+@Repository // JpaRepository에 빈으로 등록되는게 있어서, 굳이 이 어노테이션을 안 써도 됨
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // SELECT * FROM user WHERE username = 1?;
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
+
+
+// JPA Naming Query 전략
+// SELECT * FROM user WHERE username = ? AND password = ?;
+
+
+//    User findByUsernameAndPassword(String username, String password);
+//
+//    @Query(value="SELECT * FROM user WHERE username = ?1 AND password = ?2", nativeQuery = true)
+//    User login(String username, String password);
